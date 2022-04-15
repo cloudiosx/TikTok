0b5457bce7c0ab93dd87d698a7b91ca4f68be66e
John Kim <cloudiosx@gmail.com>
2022-04-15 10:02:40 -0400

    Setup tab bar controller

--- TikTok/Controllers/Core/TabBarViewController.swift
+++ TikTok/Controllers/Core/TabBarViewController.swift
@@ -11,19 +11,35 @@ class TabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        setUpTabBarController()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setUpTabBarController() {
+        let home = HomeViewController()
+        let explore = ExploreViewController()
+        let camera = CameraViewController()
+        let notifications = NotificationsViewController()
+        let profile = ProfileViewController()
+        
+        home.title = "Home"
+        explore.title = "Explore"
+        notifications.title = "Notifications"
+        profile.title = "Profile"
+        
+        let homeNavController = UINavigationController(rootViewController: home)
+        let exploreNavController = UINavigationController(rootViewController: explore)
+        let notificationsNavController = UINavigationController(rootViewController: notifications)
+        let profileNavController = UINavigationController(rootViewController: profile)
+        
+        homeNavController.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "house"), tag: 1)
+        exploreNavController.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "magnifyingglass"), tag: 1)
+        camera.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "camera"), tag: 1)
+        notificationsNavController.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "bell"), tag: 1)
+        profileNavController.tabBarItem = UITabBarItem(title: nil, image: UIImage(systemName: "person.circle"), tag: 1)
+        
+        setViewControllers([homeNavController, exploreNavController, camera, notificationsNavController, profileNavController], animated: false)
     }
-    */
 
 }
+
